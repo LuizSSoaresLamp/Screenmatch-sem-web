b3fd8df6a30f837c48a8f1e67383a02e20b65a35
LuizSSoaresLamp <158161042+LuizSSoaresLamp@users.noreply.github.com>
2024-05-03 15:25:01 -0300

    Inicio projeto Spring

---
+++ src/main/java/br/com/alura/scrennmatch/service/ConverteDados.java
@@ -0,0 +1,18 @@
+package br.com.alura.scrennmatch.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ConverteDados implements IConverteDados {
+    private ObjectMapper mapper = new ObjectMapper();
+
+
+    @Override
+    public <T> T obterDados(String Json, Class<T> classe) {
+        try {
+            return mapper.readValue(Json, classe);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
